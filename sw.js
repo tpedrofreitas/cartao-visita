c873dc381b9802d3e1c4843fa3c2b606e42767c8
Thiago Freitas <thifreitas26@gmail.com>
2026-08-17 19:28:42 -0300

    Cartao de visita

---
+++ sw.js
@@ -0,0 +1,29 @@
+const cacheName = "cartao-thiago-v1";
+
+const filesToCache = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./cartaovisita.css",
+    "./js/main.js",
+    "./icons/icon-128.png",
+    "./icons/icon-192.png",
+    "./icons/icon-256.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(filesToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
